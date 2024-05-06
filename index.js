42e84a3c2e83bea720e704a1e207a87b503616d9
nabilyamin <nabilyamin15@gmail.com>
2024-05-06 20:32:13 +0800

    sec push

--- index.js
+++ index.js
@@ -1,66 +1,67 @@
-let imageDOM = document.getElementById("image");
-let inputFile = document.getElementById("input-file");
-let submitButton = document.getElementById("submit-button");
-let formWrap = document.getElementById("form-wrap");
-let redAlert = document.getElementById("alert");
+let imageDOM = document.getElementById("image")
+let inputFile = document.getElementById("input-file")
+let submitButton = document.getElementById("submit-button")
+let formWrap = document.getElementById("form-wrap")
+let redAlert = document.getElementById("alert")
 //form
-let namaDepan = document.getElementById("nama-depan");
-let namaBelakang = document.getElementById("nama-belakang");
-const tglLahir = document.getElementById("tanggal-lahir");
-let eMail = document.getElementById("email");
-let noHP = document.getElementById("noHP");
+let namaDepan = document.getElementById("nama-depan")
+let namaBelakang = document.getElementById("nama-belakang")
+const tglLahir = document.getElementById("tanggal-lahir")
+let eMail = document.getElementById("email")
+let noHP = document.getElementById("noHP")
 
 // const reloadPage = () => {
 //     location.reload();
 // };
 
 inputFile.onchange = () => {
-    imageDOM.src = URL.createObjectURL(inputFile.files[0]);
-};
+    imageDOM.src = URL.createObjectURL(inputFile.files[0])
+}
 
 submitButton.addEventListener("click", (e) => {
-    const userTanggalLahir = tglLahir.value;
+    const userTanggalLahir = tglLahir.value
     // if (
     //     !namaDepan.value ||
     //     !namaBelakang.value ||
     //     !noHP.value ||
     //     !eMail.value
     // ) {
-    //     window.scrollTo(0, 0);
-    //     redAlert.innerHTML = `<p>Provide required credentials</p>`;
-    //     console.log({ namaDepan, namaBelakang, noHP, eMail });
+    //     window.scrollTo(0, 0)
+    //     redAlert.innerHTML = `<p>Provide required credentials</p>`
+    //     console.log({ namaDepan, namaBelakang, noHP, eMail })
     // } else {
-    redAlert.style.display = "none";
+    redAlert.style.display = "none"
     // e.preventDefault;
-    const userFrontName = namaDepan.value;
-    const userLastName = namaBelakang.value;
-    const userEmail = eMail.value;
-    const userPhoneNumber = noHP.value;
+    const userFrontName = namaDepan.value
+    const userLastName = namaBelakang.value
+    const userEmail = eMail.value
+    const userPhoneNumber = noHP.value
 
     formWrap.innerHTML = `<h2>SELAMAT BERGABUNG !</h2>
     <h2>${userFrontName.toUpperCase()} ${userLastName.toUpperCase()}</h2>
     <div id="end-showcase">
         <table style="width:100%">
             <tr>
-                <td>Nama: </td>
-                <td>${userFrontName} ${userLastName} </td>
+                <td class="table-form">Nama: </td>
+                <td class="table-cred">${userFrontName} ${userLastName} </td>
             </tr>
                 <tr>
-                <td>Tanggal Lahir: </td>
-                <td>${userTanggalLahir} </td>
+                <td class="table-form">Tanggal Lahir: </td>
+                <td class="table-cred">${userTanggalLahir} </td>
                 </tr>
                 <tr>
-                <td>Email </td>
-                <td>${userEmail} </td>
+                <td class="table-form">Email: </td>
+                <td class="table-cred">${userEmail} </td>
                 </tr>
                 <tr>
-                <td>NomorHandphone </td>
-                <td>${userPhoneNumber} </td>
+                <td class="table-form">NomorHandphone: </td>
+                <td class="table-cred">${userPhoneNumber} </td>
             </tr>
         </table>
         
-        </div>; 
+        </div> 
         <img src="${imageDOM.src} ">
         
-        <button type="button "onClick="location.reload()">Back</button>`;
-});
+        <button type="button "onClick="location.reload()">Back</button>`
+    // }
+})
